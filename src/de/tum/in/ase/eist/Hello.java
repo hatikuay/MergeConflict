0695b0e0ae27bc302e20408834a974569a822403
Yusuf Karakose <yusuf.karakoese@tum.de>
2023-07-30 16:57:24 +0300

    v1

--- src/de/tum/in/ase/eist/Hello.java
+++ src/de/tum/in/ase/eist/Hello.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello EIST");
+        System.out.println("Hello World");
     }
 }
